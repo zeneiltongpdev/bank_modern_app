c077f6fe1b0c18e6d8ac984b789ac46c523d51c8
✞Ȥɘɳɘʅ✞ <zeneiltongranja@gmail.com>
2023-06-29 14:35:14 -0300

    Added Hero File

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./style";
 
-import { Navbar } from "./components";
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
 
 
 const App = () => (
@@ -15,19 +15,19 @@ const App = () => (
 
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Hero
+        <Hero />
       </div>
     </div>
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Stats
-        Business
-        Billing
-        CardDeal
-        Testimonials
-        Clients
-        CTA
-        Footer
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
     </div>
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,23 @@
 import Navbar from "./Navbar/Navbar";
+import Billing from "./Billing/Billing";
+import CardDeal from "./CardDeal/CardDeal";
+import Business from "./Business/Business";
+import Clients from "./Clients/Clients";
+import CTA from "./CTA/CTA";
+import Stats from "./Stats/Stats";
+import Footer from "./Footer/Footer";
+import Testimonials from "./Testimonials/Testimonials";
 import Hero from "./Hero/Hero";
 
-export { Navbar, Hero };
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
