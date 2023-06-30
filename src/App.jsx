a1accea7012268ff3fe544063d56f63370972399
✞Ȥɘɳɘʅ✞ <zeneiltongranja@gmail.com>
2023-06-30 16:14:14 -0300

    deploying billing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import styles from "./style";
 
-import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
-
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from "./components";
 
 const App = () => (
-
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
@@ -30,9 +39,7 @@ const App = () => (
         <Footer />
       </div>
     </div>
-
   </div>
-
 );
 
 export default App;
